0b5dc2d9c9cdbd001a88d095cea7424fff84e8c0
esfox <osairis100@gmail.com>
2023-11-21 14:40:11 +0800

    Create CMS package

---
+++ packages/cms/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, MainLayoutComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'cms';
+}
