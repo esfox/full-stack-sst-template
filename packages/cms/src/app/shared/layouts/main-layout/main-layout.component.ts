ad60ac5b1b6774bee442edd623dd8df0f2dc08a7
esfox <osairis100@gmail.com>
2023-11-22 15:57:25 +0800

    [CMS] Create main layout

--- packages/cms/src/app/shared/layouts/main-layout/main-layout.component.ts
+++ packages/cms/src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, SidebarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
